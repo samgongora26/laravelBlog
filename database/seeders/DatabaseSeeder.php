ed2d2f5899927a652d547eb0047280e3668d0728
samgongora26 <saul.santiago2426@gmail.com>
2022-04-04 09:27:51 -0500

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+use App\Models\Post;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Saul Gongora',
+            'email' => 'sg@admin.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        Post::factory()->count(24)->create();
+    }
+}
